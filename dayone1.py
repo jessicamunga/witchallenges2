69c2ae7e665d3cec9438744f2bdff88d39dc25ee
jessicamunga <marjessica94@gmail.com>
2018-08-31 12:40:17 +0300

    initial commit

---
+++ dayone1.py
@@ -0,0 +1,13 @@
+cfrom datetime import date
+def age():
+    yob = int(input('enter your year:'))
+    today_date = date.today().year
+    age = today_date-yob 
+    if age < 18:
+        print('minor')
+    elif age >=18 and age <= 36:
+        print('youth')  
+    else:
+        print('elder')
+
+age ()
